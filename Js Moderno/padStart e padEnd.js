3c1e97dd655155f336e1ac50a384d25d4b92f3b8
Kayke-Fujinaka <kaykealvesfujinaka@gmail.com>
2022-06-20 17:50:43 -0300

    padStart

---
+++ Js Moderno/padStart e padEnd.js
@@ -0,0 +1,15 @@
+// padStart  
+// O método padStart() preenche a string original com um determinado caractere, ou conjunto de caracteres,
+// (várias vezes, se necessário) até que a string resultante atinja o comprimento fornecido.
+// O preenchimento é aplicado antes do primeiro caractere da string original.
+
+console.log('0'.padStart(10, "1"))
+
+// padEnd
+// O método padEnd() preenche a string original com um determinado caractere, ou conjunto de caraceres, 
+// (repetidamente, se necessário) para que a string resultante alcance um determinado comprimento. 
+// O preenchimento é aplicado a partir do final (direita) da string original.
+
+console.log('0'.padEnd(10, "1"))
+
+// padX(tamanhoDesejado, textoParaAdicionar)
