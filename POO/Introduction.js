b0ab26163a71d9122b2b0f503519faab84e2e1d8
Kayke-Fujinaka <kaykealvesfujinaka@gmail.com>
2022-06-24 16:41:26 -0300

    [POO] - Introduction

---
+++ POO/Introduction.js
@@ -0,0 +1,12 @@
+/* 
+"Everything in JavaScript, is an Object"
+- Objects no JavaScript são semelhantes com objetos na vida real, porém aqui eles possuem propriedade e coisas que podem realizas (métodos)
+
+Anotação
+- Criar o objeto usando Notação de nbjeto literal. Ex: let a = {}
+- JavaScript Classes e Construtores
+- Herança
+- Método de encadeamento
+- Protótipo e Herança Prototipal
+
+*/
